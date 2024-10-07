e1b6fed6c31aeee507beef96cbbc4aaace5f291f
GirinathRG <girinathr@simplfin.tech>
2024-10-07 19:55:48 +0530

    Resolved merge conflicts

--- bot_world/wsgi.py
+++ bot_world/wsgi.py
@@ -8,11 +8,7 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bot_world.settings")
-
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bot_world.settings')
 application = get_wsgi_application()
-
-app = application
--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,11 @@
- echo "BUILD START"
- python3.11 -m pip install -r requirements.txt
- python3.11 manage.py collectstatic --noinput --clear
- echo "BUILD END"
+#!/bin/bash
+
+echo "BUILD START"
+
+# Install the required dependencies from requirements.txt
+python3.11 -m pip install -r requirements.txt
+
+# Run the Django collectstatic command to collect static files
+python3.11 manage.py collectstatic --noinput --clear
+
+echo "BUILD END"
